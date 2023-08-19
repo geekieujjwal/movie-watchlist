aa69071dd09d134ebf842d99395c303875c9362f
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-08-20 02:21:20 +0530

    initial:init

---
+++ index.js
@@ -0,0 +1,91 @@
+const movieList = document.getElementById("movieList");
+const inputEl = document.getElementById("inputEl");
+const searchBtn = document.getElementById("search-btn");
+const loader = document.querySelector(".loader");
+
+const getMovie = async () => {
+  // Fetching MovieData after taking user Input
+  const res = await fetch(
+    `http://www.omdbapi.com/?s=${inputEl.value}&plot=full&apikey=f230991a`
+  );
+  const data = await res.json();
+
+  // If there is no movie, then unable to find in Loader
+  if (data.Search === undefined) {
+    loader.style.fontSize = "1.8rem";
+    loader.textContent = `Unable to find what you're looking for. Please try another search.`;
+  } else {
+    inputEl.value = "";
+    movieList.innerHTML = "";
+
+    // Iterating over the array data.Search
+    data.Search.forEach((movie) => {
+      //
+      //Fetching Movies after updating the URL()
+      const getMovieDetails = async () => {
+        const res = await fetch(
+          `http://www.omdbapi.com/?t=${movie.Title}&apikey=f230991a`
+        );
+        const dataOfMovie = await res.json();
+        loader.textContent = "";
+
+        movieList.innerHTML += `
+        <div class = "movies">
+            <div class = "movie-img">
+                <img src = "${dataOfMovie.Poster}"/>
+            </div>
+            <div class = "movie-about">
+                <div>
+                    <span class = "movie-name">${dataOfMovie.Title}</span>
+                    <i class="fa-solid fa-star" style="color: #f5e10a;"></i>
+                    <span class = "movie-rating">${dataOfMovie.imdbRating}</span>
+                 </div>
+                <div class = "movie-brief">
+                    <span>${dataOfMovie.Runtime}</span>
+                    <span>${dataOfMovie.Genre}</span>
+                    <div class = "plus-watchlist">
+                        <div id = "plus-icon" data-plus = "${dataOfMovie.imdbID}">+</div>
+                        <span>Watchlist</span>
+                    </div>
+                </div>
+                <div class = "movie-description">
+                  ${dataOfMovie.Plot}
+                </div>
+            </div>
+        </div>
+        <hr>
+        `;
+      };
+      getMovieDetails();
+    });
+  }
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.plus) {
+    e.target.parentElement.innerHTML = `
+                    <div class = "plus-watchlist">
+                        <div><i class="fa-solid fa-check" style="color: #15f424;"></i></div>
+                        <span>Added to Watchlist!</span>
+                    </div>
+                    `;
+    const arrayOfId = JSON.parse(localStorage.getItem("movie")) ?? [];
+    console.log(arrayOfId);
+    if (
+      e.target.dataset.plus !== "undefined" &&
+      !arrayOfId.includes(e.target.dataset.plus)
+    ) {
+      arrayOfId.push(e.target.dataset.plus);
+    }
+    console.log(arrayOfId);
+    localStorage.setItem("movie", JSON.stringify(arrayOfId));
+  }
+});
+
+searchBtn.addEventListener("click", getMovie);
+
+inputEl.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    searchBtn.click();
+  }
+});
---
+++ watchlist.js
@@ -0,0 +1,71 @@
+//
+const movieList = document.getElementById("movieList");
+const loader = document.querySelector(".loader");
+let arrayOfId = JSON.parse(localStorage.getItem("movie"));
+console.log(arrayOfId);
+arrayOfId.length > 0 && loader.classList.add("hidden");
+
+const getWatchlistMovie = (array) => {
+  movieList.innerHTML = "";
+  array.forEach(async (movieID) => {
+    const res = await fetch(
+      `http://www.omdbapi.com/?i=${movieID}&apikey=f230991a`
+    );
+    const watchlistedMovies = await res.json();
+    movieList.innerHTML += `
+        <div class = "movies">
+            <div class = "movie-img">
+                <img src = "${watchlistedMovies.Poster}"/>
+            </div>
+            <div class = "movie-about">
+                <div>
+                    <span class = "movie-name">${watchlistedMovies.Title}</span>
+                    <i class="fa-solid fa-star" style="color: #f5e10a;"></i>
+                    <span class = "movie-rating">${watchlistedMovies.imdbRating}</span>
+                 </div>
+                <div class = "movie-brief">
+                    <span>${watchlistedMovies.Runtime}</span>
+                    <span>${watchlistedMovies.Genre}</span>
+                    <div class = "plus-watchlist">
+                        <div id = "plus-icon" data-minus = "${watchlistedMovies.imdbID}">-</div>
+                        <span>Remove from Watchlist</span>
+                    </div>
+                </div>
+                <div class = "movie-description">
+                  ${watchlistedMovies.Plot}
+                </div>
+            </div>
+        </div>
+        <hr>
+        `;
+  });
+};
+getWatchlistMovie(arrayOfId);
+
+const WatchlistMovieEmptyOrNot = () => {
+  if (!arrayOfId.length > 0) {
+    movieList.innerHTML = `
+            <div class="loader">
+                <p>Your watchlist is looking a little empty ...</p>
+                <div id="add-movies">
+                  <div id="plus-icon2"><a href="/index.html">+</a></div>
+                  <p>Let's add some movies!</p>
+                </div>
+            </div>
+            `;
+  }
+};
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.minus) {
+    console.log(e.target.dataset.minus);
+    const index = arrayOfId.indexOf(e.target.dataset.minus);
+    if (index > -1) {
+      arrayOfId.splice(index, 1);
+    }
+    console.log(arrayOfId);
+    getWatchlistMovie(arrayOfId);
+    WatchlistMovieEmptyOrNot();
+    localStorage.setItem("movie", JSON.stringify(arrayOfId));
+  }
+});
